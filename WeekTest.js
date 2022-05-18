d1f2b4b62740f3aa1f2b1344509150040ed5291f
SAROJ KUMAR SAHOO <85055860+SAROJ-2207@users.noreply.github.com>
2022-05-19 00:48:36 +0530

    Added :- To read a number and display the week day

---
+++ WeekTest.js
@@ -0,0 +1,13 @@
+
+    let num = 9;
+    var weekList = ["Sunday","Monday","Tuesday","Wednesday","Thurshday","Friday","Saturday"];
+    for(let i = 0; i < 7 ; i ++){
+        if((num - 1) == i ){
+        console.log(weekList[i]+" ");
+        console.log(num)
+    }else{
+        console.log("Wrong Input Try Agay...")
+    }
+  }
+
+
