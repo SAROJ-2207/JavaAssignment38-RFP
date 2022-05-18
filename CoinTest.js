897618e48b42ccfaea8849a0866e8f7c2700e2aa
SAROJ KUMAR SAHOO <85055860+SAROJ-2207@users.noreply.github.com>
2022-05-19 00:43:56 +0530

    Added :- To Coin Flip And Print out "Head" or "Tail"

---
+++ CoinTest.js
@@ -0,0 +1,12 @@
+function choice(num){
+       
+        if(num == 0){
+        console.log("Head")
+        }else if(num == 1){
+        console.log("Tail")
+       }else{
+        console.log("Wrong Input...")
+    }
+  }
+
+choice(5)
