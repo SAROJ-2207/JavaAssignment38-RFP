dd50dbf136b7622c7437317fadd2fd2eff771523
SAROJ KUMAR SAHOO <85055860+SAROJ-2207@users.noreply.github.com>
2022-05-19 00:46:41 +0530

    Added :- To read a single digit number convert a word

---
+++ NumberConWords.js
@@ -0,0 +1,34 @@
+function wordNum(num) {
+switch(num) {
+    case 1:
+        console.log(num+" One");
+        break;
+    case 2: 
+        console.log(num+" Two");
+        break;
+    case 3:
+        console.log(num+ " Three ");    
+        break;
+    case 4:
+        console.log(num+" Four");
+        break;
+    case 5:
+        console.log(num+ " Five");
+        break;
+    case 6:
+        console.log(num+" Six");
+        break;
+    case 7: 
+        console.log(num+" Seven");
+        break;
+    case 8:
+        console.log(num+ " Eight");
+        break;
+    case 9:
+        console.log(num+" Nine");
+        break;    
+    default:
+        console.log("Wrong Input Please Try Again...");
+    }
+}
+wordNum(5);
